802309784b81e48d24ab8e0545b476cf73548d8a
Miguel Angel Piza <miguelpiza@192.168.1.9>
2024-05-15 16:58:08 -0500

    Update the active logic for sidebarlink

--- src/components/Sidebar/SidebarLink/SidebarLink.jsx
+++ src/components/Sidebar/SidebarLink/SidebarLink.jsx
@@ -1,28 +1,18 @@
 import cn from 'classnames';
 import PropTypes from 'prop-types';
-import { matchPath, NavLink, useLocation } from 'react-router-dom';
+import { useMatch, NavLink } from 'react-router-dom';
 import SidebarItem from '../SidebarItem';
 import styles from './SidebarLink.module.scss';
 
-const SidebarLink = ({ icon, iconActive, label, href, rightBadge, childRoutes }) => {
-  const location = useLocation();
-
-  // Taken from https://github.com/remix-run/react-router/blob/v5.3.0/packages/react-router-dom/modules/NavLink.js#L56
-  const escapedPath = href && href.replace(/([.+*?=^!:${}()[\]|/\\])/g, "\\$1");
-
-  const match = matchPath({
-    path: escapedPath,
-    exact: false
-  }, location.pathname);
-
+const SidebarLink = ({ icon, label, href, rightBadge, childRoutes }) => {
+  const match = useMatch(`${href}/*`);
   const hasSubRoutes = !!childRoutes?.length;
-
   return (
     <>
       <NavLink className={styles.item} to={href}>
         <SidebarItem
           className={cn({ [styles.active]: match, [styles.withSubRoutes]: hasSubRoutes })}
-          icon={match ? iconActive : icon}
+          icon={icon}
           label={label}
           rightBadge={rightBadge}
         />
@@ -49,7 +39,6 @@ const SidebarLink = ({ icon, iconActive, label, href, rightBadge, childRoutes })
 
 SidebarLink.propTypes = {
   icon: PropTypes.elementType.isRequired,
-  iconActive: PropTypes.elementType.isRequired,
   label: PropTypes.string.isRequired,
   href: PropTypes.string.isRequired,
   rightBadge: PropTypes.node
