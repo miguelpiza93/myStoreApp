e9e09cf7eda5ce1b87d2e6d4e9536b7a113bc3a4
Miguel Angel Piza <mapiz_930@hotmail.com>
2024-05-27 14:40:45 -0500

    Update logic

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -2,27 +2,40 @@ import cn from 'classnames';
 import styles from "./LeftSidebar.module.scss";
 import SidebarLink from "./SidebarLink"
 import { getAppSideBarRoutes } from '../../routes/constants/appRoutes'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Sidebar = ({ className }) => {
   const routes = getAppSideBarRoutes();
 
-  const [state, setState] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [mode, setMode] = useState("FIXED");
 
-  const build = (isCollapsable) => {
-    return <aside className={cn(className, styles.sidebar, isCollapsable ? styles.collapsable : styles.fixed)}>
-      {isCollapsable && <button onClick={() => setState(!state)}>{state ? "Close" : "Open"}</button>}
-      {(state || !isCollapsable) && routes.map(routeProps => (
+  useEffect(() => {
+    if (mode === "FIXED") {
+      setIsOpen(false);
+    }
+  }, [mode]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setMode(window.innerWidth >= 800 ? "FIXED" : "COLLAPSABLE");
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <aside className={cn(className, styles.sidebar)}>
+      {mode === "COLLAPSABLE" && <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? "Close" : "Open"}</button>}
+      {(isOpen || mode === "FIXED") && routes.map(routeProps => (
         <SidebarLink className={styles.sideBarLink} key={routeProps.label} {...routeProps} />
       ))}
     </aside>
-  }
-
-  return (
-    <>
-      {build(true)}
-      {build(false)}
-    </>
   );
 };
 
