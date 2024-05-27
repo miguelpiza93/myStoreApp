faf7c989f535dceb07c56043b7da1ba6db37f522
Miguel Angel Piza <mapiz_930@hotmail.com>
2024-05-27 14:21:10 -0500

    First try

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -2,15 +2,27 @@ import cn from 'classnames';
 import styles from "./LeftSidebar.module.scss";
 import SidebarLink from "./SidebarLink"
 import { getAppSideBarRoutes } from '../../routes/constants/appRoutes'
+import { useState } from 'react';
 
 const Sidebar = ({ className }) => {
   const routes = getAppSideBarRoutes();
-  return (
-    <aside className={cn(className, styles.sidebar)}>
-      {routes.map(routeProps => (
+
+  const [state, setState] = useState(false);
+
+  const build = (isCollapsable) => {
+    return <aside className={cn(className, styles.sidebar, isCollapsable ? styles.collapsable : styles.fixed)}>
+      {isCollapsable && <button onClick={() => setState(!state)}>{state ? "Close" : "Open"}</button>}
+      {(state || !isCollapsable) && routes.map(routeProps => (
         <SidebarLink className={styles.sideBarLink} key={routeProps.label} {...routeProps} />
       ))}
     </aside>
+  }
+
+  return (
+    <>
+      {build(true)}
+      {build(false)}
+    </>
   );
 };
 
