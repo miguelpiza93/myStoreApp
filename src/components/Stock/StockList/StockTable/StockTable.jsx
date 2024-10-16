8e61436145f23af1430c1e873bf26016b8739591
Miguel Angel Piza <mapiz_930@hotmail.com>
2024-10-16 14:35:12 -0500

    Remove sale price from stock item

--- src/components/Stock/StockList/StockTable/StockTable.jsx
+++ src/components/Stock/StockList/StockTable/StockTable.jsx
@@ -1,19 +1,12 @@
 import { useGetStockSummaryQuery } from "../../../../api/stock/stockApi";
-import { useSetSalePriceMutation } from "../../../../api/product/productApi";
 import Table from '../../../Table';
 
 const StockTable = ({ className }) => {
   const { data: stock, error, isLoading } = useGetStockSummaryQuery();
-  const [setProductSalePrice] = useSetSalePriceMutation();
-
 
   if (isLoading) return <div>Loading...</div>
   if (!stock) return <div>Missing stock!</div>
   if (error) return <div>Error getting stock!</div>
-
-  const onEdit = (data) => {
-    setProductSalePrice(data);
-  };
 
   return (
     <Table
@@ -31,19 +24,12 @@ const StockTable = ({ className }) => {
           {
             label: 'Costo Ponderado',
             accessor: 'weightedCost'
-          },
-          {
-            label: 'Precio de venta',
-            accessor: 'salePrice',
-            isEditable: true,
-            type: 'number',
           }
         ]
       }
       data={stock.map(item => {
         return { id: item.productId, ...item }
       })}
-      onEdit={onEdit}
     />
   );
 };
