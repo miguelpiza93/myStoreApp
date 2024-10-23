19ff0fad6d731ef06c4fd8f6eb0c0a244a6a730c
Miguel Angel Piza <mapiz_930@hotmail.com>
2024-10-23 09:45:47 -0500

    Add selected product table in register sale

--- src/components/Sales/RegisterSale/RegisterSale.jsx
+++ src/components/Sales/RegisterSale/RegisterSale.jsx
@@ -1,10 +1,14 @@
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGetStockSummaryQuery } from "../../../api/stock/stockApi";
 import SearchableDropdown from "../../SearchableDropdown";
+import Table from '../../Table';
 
 const RegisterSale = () => {
     const navigate = useNavigate();
     const { data, error, isLoading } = useGetStockSummaryQuery();
+
+    const [selectedProducts, setSelectedProducts] = useState([]);
 
     if (isLoading) return <div>Loading...</div>
     if (!data) return <div>Missing stock!</div>
@@ -15,17 +19,45 @@ const RegisterSale = () => {
     }
 
     const handleSelect = (item) => {
-        console.log("Selected item:", item);
+        setSelectedProducts([...selectedProducts, item])
     };
+
+    const stockData = data.map(stockItem => { 
+        return { 
+            ...stockItem, 
+            id: stockItem.vendorProductId,
+            availableQuantity: stockItem.quantity,
+        } 
+    });
 
     return (
         <div>
             <h1>Registrar Venta</h1>
             <SearchableDropdown
                 placeholder="Search for a product..."
-                data={data.map(stockItem => { return { ...stockItem, id: stockItem.vendorProductId } })}
+                data={stockData}
                 searchField="fullDescription"
                 onSelect={handleSelect}
+            />
+            <Table
+                columns={
+                    [
+                        {
+                            label: 'Item',
+                            accessor: 'fullDescription'
+                        },
+                        {
+                            label: 'Cantidad',
+                        },
+                        {
+                            label: 'Precio Unitario',
+                        },
+                        {
+                            label: 'Total',
+                        }
+                    ]
+                }
+                data={selectedProducts}
             />
             <button onClick={handleRegisterSale} aria-labelledby="submit">
                 Guardar
